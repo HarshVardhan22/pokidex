006b5442248f81bdcb550eb38ec638f347916e20
Harshvardhan <67060748+HarshVardhan22@users.noreply.github.com>
2021-10-06 10:21:06 +0530

    +pokemon search by name

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
 import { useState, useEffect } from "react";
 import Card from "./Components/Card";
+import Details from "./Components/Details";
 export default function App() {
   const [data, setData] = useState();
   const [limit, setLimit] = useState(10);
-
+  const [inputValue, setInputValue] = useState();
   const [pokiResult, setPokiResult] = useState();
+  const[pokemon, setPokemon]  = useState();
+
   const [url, setUrl] = useState(
     `https://pokeapi.co/api/v2/pokemon?offset=200&limit=${limit}`
   );
@@ -29,16 +32,28 @@ export default function App() {
 
   const applyLimit = (limit) => {
     let indexOfLimit = url.indexOf("limit=");
-    let str = url.substring(0, indexOfLimit);
-    console.log(str + `limit=${limit}`);
-    setUrl(url.substring(0, indexOfLimit) + `limit=${limit}`);
+    if (indexOfLimit !== -1) {
+      let str = url.substring(0, indexOfLimit);
+      console.log(str + `limit=${limit}`);
+      setUrl(url.substring(0, indexOfLimit) + `limit=${limit}`);
+    }
+  };
+
+  const searchByName = async () => {
+    setUrl(`https://pokeapi.co/api/v2/pokemon/${inputValue}`);
   };
 
   const fetchData = async () => {
     const res = await fetch(url);
     const dataFetched = await res.json();
     setData(dataFetched);
-    setPokiResult(dataFetched.results);
+    
+    if(dataFetched.results!== undefined)
+   { setPokiResult(dataFetched.results);}
+    else
+
+  { console.log(dataFetched) 
+    setPokemon(dataFetched)}
   };
 
   const next = () => {
@@ -60,6 +75,14 @@ export default function App() {
         <button onClick={next}>Next</button>
         <button onClick={handleSort}>Sort By Name</button>
 
+        <input
+          type="text"
+          onChange={(e) => setInputValue(e.target.value)}
+          placeholder="Enter name of pokemon"
+          value={inputValue}
+        />
+        <button onClick={searchByName}>Search</button>
+
         <select onChange={handleLimit}>
           <option value="">Select number of pokemon</option>
           <option value="10">10</option>
@@ -77,6 +100,10 @@ export default function App() {
         <button onClick={prev}>Prev</button>
         <button onClick={next}>Next</button>
       </footer>
+
+    {pokemon && <Details pokemon={pokemon}/>
+      
+   }
     </div>
   );
 }
--- src/Components/Details.jsx
+++ src/Components/Details.jsx
@@ -1,12 +1,36 @@
-import React from 'react'
-import './Details.css'
+import React, { useState } from "react";
+import "./Details.css";
 
-const Details = () => {
-    return (
-        <div>
-                        
-        </div>
-    )
-}
+const Details = (pokemon) => {
+  const [images, setImages] = useState(pokemon.pokemon?.sprites);
+  const [keys, setKeys] = useState(pokemon.pokemon.sprites?Object.keys(pokemon.pokemon.sprites):[]);
+  const [index, setIndex] = useState(0);
+  console.log(images,keys)
+  return (
+    <div className="details">
+      <button
+        onClick={(e) => {
+          setIndex(index + 1);
+        }}
+        disabled={index === keys.length - 1 ? true : false}
+      >
+        Next Image
+      </button>
+      <img src={images[keys[index]]} alt={pokemon.pokemon.name} />
+      <button
+        onClick={(e) => {
+          setIndex(index + 1);
+        }}
+        disabled={index === 0 ? true : false}
+      >
+        Prev Image
+      </button>
+      <h3>Name : {pokemon.pokemon.name}</h3>
 
-export default Details
+      <h3>Height : {pokemon.pokemon.height}</h3>
+      <h3>Weight : {pokemon.pokemon.weight}</h3>
+    </div>
+  );
+};
+
+export default Details;
