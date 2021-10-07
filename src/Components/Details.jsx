4b86abb9b0c130ac750c51bba0f03b86d5b6103e
Harshvardhan <67060748+HarshVardhan22@users.noreply.github.com>
2021-10-07 12:41:46 +0530

    +fixed overflow issue of abilites and moves

--- src/Components/Details.jsx
+++ src/Components/Details.jsx
@@ -8,7 +8,7 @@ const Details = () => {
   const location = useLocation();
   // eslint-disable-next-line
   const [pokemonSelected, setPokemonSelected] = useState(location.state);
- // eslint-disable-next-line
+  // eslint-disable-next-line
   const [values, setValues] = useState(
     pokemonSelected.sprites
       ? Object.values(pokemonSelected.sprites).filter(
@@ -17,62 +17,63 @@ const Details = () => {
       : []
   );
   const [index, setIndex] = useState(0);
-  const abilities = pokemonSelected.abilities 
-  const moves = pokemonSelected.moves
+  const abilities = pokemonSelected.abilities;
+  const moves = pokemonSelected.moves;
 
   return (
     <div className="details__main">
       <div className="details">
         <div className="details__container">
           <div className="details__left">
-          
-          <img src={values[index]} alt={pokemonSelected.name} />
-            
-    
-          
-            
-        <div className="details__btnContainer">
-        <Button
-              variant="contained"
-              size="small"
-              sx={{ mt: 2,mr:5 }}
-              onClick={(e) => {
-                setIndex(index - 1);
-              }}
-              disabled={index === 0 ? true : false}
-            >
-              Prev Image
-            </Button>
-        <Button
-              variant="contained"
-              size="small"
-              sx={{ mt: 2, ml:5 }}
-              onClick={(e) => {
-                setIndex(index + 1);
-              }}
-              disabled={index === values.length - 1 ? true : false}
-            >
-              Next Image
-            </Button>
+            <img src={values[index]} alt={pokemonSelected.name} />
 
-          
-        </div>
+            <div className="details__btnContainer">
+              <Button
+                variant="contained"
+                size="small"
+                sx={{ mt: 2, mr: 5 }}
+                onClick={(e) => {
+                  setIndex(index - 1);
+                }}
+                disabled={index === 0 ? true : false}
+              >
+                Prev Image
+              </Button>
+              <Button
+                variant="contained"
+                size="small"
+                sx={{ mt: 2, ml: 5 }}
+                onClick={(e) => {
+                  setIndex(index + 1);
+                }}
+                disabled={index === values.length - 1 ? true : false}
+              >
+                Next Image
+              </Button>
+            </div>
           </div>
           <div className="details__right">
             {" "}
             <h3>Name : {pokemonSelected.name}</h3>
             <h3>Height : {pokemonSelected.height}</h3>
             <h3>Weight : {pokemonSelected.weight}</h3>
+            <div className="details__subContainer">
             <h3>Moves: </h3>
-          {moves?.map(item=>{
-              return <p>{item.move.name}</p>
-          })}
-          <h3>Abilities: </h3>
-          {abilities?.map(item=>{
-              return <p>{item.ability.name}</p>
-          })}
+              <div className="details__moves">
+                
+                {moves?.map((item) => {
+                  return <p>{item.move.name}</p>;
+                })}
+              </div>
+              <h3>Abilities: </h3>
+              <div className="details__ability">
+                
+                {abilities?.map((item) => {
+                  return <p>{item.ability.name}</p>;
+                })}
+              </div>
+            </div>
           </div>
-         
         </div>
         <Link to="/">
           <Button variant="contained" size="small" sx={{ mt: 2 }}>
